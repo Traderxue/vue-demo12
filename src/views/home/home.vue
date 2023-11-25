471fdc1f99c282f3765a48cfdb1d384419d254d2
Traderxue <falethe@gmail.com>
2023-11-25 16:31:26 +0800

    init

---
+++ src/views/home/home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home">
+        <div class="header">
+            <span></span><span></span><span></span>
+        </div>                        
+    </div>
+</template>
---
+++ src/views/index.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+
+const active = ref(0);
+</script>
+
+<template>
+  <div class="index">
+    <RouterView />
+    <van-tabbar v-model="active" inactive-color="#f5f5f5" active-color="#3ec1d3" route>
+      <van-tabbar-item to="/home">
+        <span class="material-symbols-outlined"> home </span><br />
+        <span>首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/quote">
+        <span class="material-symbols-outlined"> monitoring </span><br />
+        <span>行情</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/wallet">
+        <span class="material-symbols-outlined"> account_balance_wallet </span
+        ><br />
+        <span>钱包</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/mine">
+        <span class="material-symbols-outlined"> person </span><br />
+        <span>我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.index{
+  width: auto;
+  height: auto;
+  padding: 15px;
+}
+:deep(.van-tabbar){
+  background: #364f6b;
+  height: 55px;
+}
+:deep(.van-tabbar-item){
+  background: #364f6b;
+}
+</style>

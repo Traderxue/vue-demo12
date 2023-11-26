e31856d1ab91ce1cfbdc2bf2b4024c3329ac1fc0
Traderxue <falethe@gmail.com>
2023-11-26 17:33:08 +0800

    init

--- src/views/mine/mine.vue
+++ src/views/mine/mine.vue
@@ -1,3 +1,46 @@
 <template>
-    <div class="mine">mine</div>
+    <div class="mine">
+        <div class="header">
+            <span>我的</span>
+        </div>
+        <div class="banner">
+            <img src="https://avatars.githubusercontent.com/u/128983356" alt="">
+            <span>ID: 106412</span>
+            <span>信用分: 100</span>
+        </div>
+        <div class="box">
+            <div class="per">
+                
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.mine{
+    width: auto;
+    height: auto;
+    color: #FFF;
+    .header{
+        text-align: center;
+    }
+    .banner{
+        width: auto;
+        height: auto;
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 40px;
+        img{
+            width: 70px;
+            height: 70px;
+            border-radius: 50%;            
+            margin-bottom: 20px;
+        }
+        span{
+            margin: 5px 0px;
+        }
+    }
+}
+</style>
